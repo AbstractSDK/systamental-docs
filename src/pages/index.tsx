5bea9567cf4c98ba399f495b675dfb34b9a7d1b7
adairrr <32375605+adairrr@users.noreply.github.com>
2025-04-01 20:09:47 -0400

    Add search and start notion integration

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -21,7 +21,7 @@ function HomepageHeader() {
           <Link
             className="button button--secondary button--lg"
             to="/docs/introduction">
-            Docusaurus Tutorial - 5min ⏱️
+            Start Investing Better
           </Link>
         </div>
       </div>
